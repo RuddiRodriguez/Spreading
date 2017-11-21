0ddd68b236a91e2d7c60f641f273530e3956cfe0
Ruddi Rodriguez Garcia <rudiscu@gmail.com>
2017-11-21 11:51:51 +0100

    Initial Commit_working

---
+++ initialization_infinite.m
@@ -0,0 +1,39 @@
+function [np,MTarryocupationtemp ] = initialization_infinite(ocupationnumber,numberpb,MTarryocupationtempindex,MTarryocupation)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+     
+    
+    
+    [~,colocunozero] = (find (ocupationnumber~=0));
+    
+    try 
+     MTLindex = (ceil ((colocunozero(end))))-(0:(numberpb-0));   
+%      MTLindexgpu=gpuArray  (MTLindex) ;
+%      MTarryocupationtempindexgpu=gpuArray(MTarryocupationtempindex);
+    C = sort(MTarryocupationtempindex(ismembc(MTarryocupationtempindex,sort(MTLindex(MTLindex>0)))));
+    %C = tmp([~~diff(tmp),true]);
+%     C = intersect(MTarryocupationtempindex,MTLindex(MTLindex>0));
+%     CC = setdiff(MTarryocupationtempindex,MTLindex(MTLindex>0)); 
+%     CCC= setdiff(1:max(C),[CC C]); 
+    np = length (C); 
+     
+     
+%     MTarryocupationtemp = MTarryocupation(MTarryocupation~=0);   
+    catch err 
+        ocupationnumber(1)=1; 
+        [~,colocunozero] = (find (ocupationnumber~=0)); 
+        MTLindex = (ceil ((colocunozero(end))))-(0:(numberpb-0)); 
+        np = length (MTarryocupationtempindex(end-(numberpb-1):end)); 
+    end 
+    
+    
+    MTarryocupationtemp = MTarryocupation(MTarryocupation~=0);              
+    
+    if length (MTarryocupationtemp)< 2
+        
+        np=np;
+    end
+   
+end
+
