6a9f970ecef0935890645200670d10d5a2a9a7b6
Ruddi Rodriguez Garcia <rudiscu@gmail.com>
2018-01-29 16:47:18 +0100

    Fix the trnasition flux zero

--- diff_mem_left.m
+++ diff_mem_left.m
@@ -11,32 +11,32 @@ if  positiontran~=0   && ocupationnumber (positiontran)~=0
     if  positiontran~=1 && ocupationnumber (positiontran-1)<5 && positiontran~=2
         new_pos = pos(end) + 0;
         iarraysize = iarraysize+0 ;
-        try
+        
         ocupationnumber (positiontran-1:positiontran+1) = ocupationnumber (positiontran-1:positiontran+1)+matrix_tmemla (4,:);
-        catch err
-            return
-        end
-        if positiontran==2 && ocupationnumber (positiontran-1)<5
-            new_pos = pos(end) + 0;
-            iarraysize = iarraysize+0 ;
-            ocupationnumber = ocupationnumber (positiontran-1:positiontran)+matrix_tmemlap1 (4,:);
-        end
     end
-    if  positiontran==1 
+    
+    
+    if positiontran==2 && ocupationnumber (positiontran-1)<5
+        new_pos = pos(end) + 0;
+        iarraysize = iarraysize+0 ;
+        ocupationnumber(positiontran-1:positiontran) = ocupationnumber (positiontran-1:positiontran)+matrix_tmemlap1 (4,:);
+    end
+    
+    if  positiontran==1
         new_pos = pos(end) + 0;
         iarraysize = iarraysize+0 ;
         ocupationnumber = ocupationnumbertemp;%ocupationnumber (positiontran-1:positiontran)+matrix_tmemlap1 (4,:);
     end
-%     if positiontran==2
-%         [controldensityneww] =density_calculation(MTarryocupation,ocupationnumber,new_pos);
-%         if controldensityneww <=(controldensitynew-5) || controldensityneww >=(controldensitynew+5)
-%             ocupationnumber =ocupationnumber;%here it is possible to use a constrain with ocupationnumbertemp to control the density
-%         else
-%             ocupationnumber =ocupationnumber;
-%         end
-%         
-%         
-%     end
+    %     if positiontran==2
+    %         [controldensityneww] =density_calculation(MTarryocupation,ocupationnumber,new_pos);
+    %         if controldensityneww <=(controldensitynew-5) || controldensityneww >=(controldensitynew+5)
+    %             ocupationnumber =ocupationnumber;%here it is possible to use a constrain with ocupationnumbertemp to control the density
+    %         else
+    %             ocupationnumber =ocupationnumber;
+    %         end
+    %
+    %
+    %     end
     tranflag=4;
 end
 end
